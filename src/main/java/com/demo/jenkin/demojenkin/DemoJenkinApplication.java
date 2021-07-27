eb8c55c9c5448ecb907f7c9c9f62318e1c6c42b9
KRIOS Nitin Tile <KRIOS_NitinTile@welspun.com>
2021-07-27 23:16:37 +0530

    commiting first time

---
+++ src/main/java/com/demo/jenkin/demojenkin/DemoJenkinApplication.java
@@ -0,0 +1,13 @@
+package com.demo.jenkin.demojenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenkinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/jenkin/demojenkin/DemoJenkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.jenkin.demojenkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
